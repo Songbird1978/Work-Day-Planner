d9995e409dbf51918745e2f83a6f9bb414babdbe
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-02-01 13:18:22 +0000

    created grid (bootstrap HTML), defined variables, added current day, added time tags (html)

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 
 
 var currentDay = $("#currentDay");
-
-
+var row = $(".row");
+var timeRightColumn = $(".col");
+var saveLeftColumn = $("#rightButton")
 var today = moment().format("D MMM YYYY");
+var thisMoment = moment().format("D MMM YYYY, HH:mm:ss");
+var thisHour = moment().format("HH");
+var rowTime = document.getElementsByTagName('time')[0].getAttribute('datetime');
+
+
+
+
+console.log(thisHour);
+console.log(rowTime);
+console.log(thisMoment);
  
+
 currentDay.text("Today's Date is : " + today);
+
+
