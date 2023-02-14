fa2deddd417bc792f0525adbb234025fcda983de
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-02-14 14:11:12 +0000

    linked data-time as array, created a foreach function for all times, created a set of if statements for the colours of the hours and created the input forms on each row - still need to add local storage saving and then this is finished

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
+$(document).ready(function () {
 
+    currentDay.text("Today's Date is : " + today); //displays today's date on the page
 
+});
+
+var inputWork = $(".col-6");
 var currentDay = $("#currentDay");
-
-var timeRightColumn = $(".col");
-var saveLeftColumn = $("#rightButton")
+var timeLeftColumn = $(".col");
+var saveRightColumn = $("#rightButton");
 var today = moment().format("D MMM YYYY");
 var thisMoment = moment().format("D MMM YYYY, HH:mm:ss");
-var thisHour = moment().format("HH:mm");
-//var rowTime = document.getElementsByTagName('time')[0].getAttribute('datetime');
-var datetime = parseInt($("#datetime").val);
+var thisHour = moment().format("HH");
+var saveBtn = $("#rightbutton");
 
-var saveBtn =  $("#rightbutton");
 
-console.log(thisHour); 
-//console.log(rowTime);
-console.log(thisMoment);
- 
+const timeJ = $("#time");
 
-currentDay.text("Today's Date is : " + today); //displays today's date on the page
+console.log(timeJ);
+
+var rowId = document.getElementById("tableTime").getAttribute("datetime");
+
+var timeNodes = document.querySelectorAll('time');
+
+//
+
+console.log(timeNodes);
 
 
 //function to check the time of day and colour each row according to past, present or future.
 //function checkTimeOfDay() {
 //select all time tags
-const row = $(".col-6"); //creates an array of col-6 for adding colour to biggest row element 
-console.log(row);
+
+timeNodes.forEach(function (timeNode) {
+
+    console.log(timeNode.dataset.time);
+
+    var hour = timeNode.dataset.time;
+    var input = timeNode.parentElement.children[1];
+
+    if (hour === thisHour) {
+        console.log("red");
+        input.classList.add("now");
+    } else if (hour < thisHour) {
+        console.log("grey");
+        input.classList.add("past");
+    } else if (hour > thisHour) {
+        console.log("green");
+        input.classList.add("future");
+    }
+
+});
 
 
 
 
 
-for (let index = 0; index < row.length; index++) {
-       
-};
-
-console.log (datetime);
-
-/*
-if(thisHour === datetime )
-row.classList.add("now");
-else if (thisHour > tableTime)
-row.classList.add("past");
-else if (thisHour < tableTime)
-row.classList.add("future");
 
 
-};
 
-*/
-
-//checkTimeOfDay();
